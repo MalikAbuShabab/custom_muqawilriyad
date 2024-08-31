50b5b8d81890d9c3e39eec8e3aec05f4004f9dc0
Malik Abo Shabab <malikabushabab@gmail.com>
2024-08-31 16:58:29 +0000

    update

--- custom_muqawilriyad/models/res_partner.py
+++ custom_muqawilriyad/models/res_partner.py
@@ -8,12 +8,23 @@ class ResPartner(models.Model):
     capital = fields.Float(string="Capital")
     commercial_register_date = fields.Date(string="Commercial Register Date")
     url = fields.Char(string="URL")
+    
 
     # Define the many2one relationship
     activity_type_ids = fields.Many2many(
         'company.activity',  # Model to which this field relates
         string="Activity Type"
     )
+
+    _sql_constraints = [
+        ('registration_number_unique', 'unique(registration_number)', 'The Activity Registration Number value must be unique!'),
+    ]
+
+    @api.constrains('registration_number')
+    def _check_registration_number(self):
+        for record in self:
+            if self.search_count([('registration_number', '=', record.registration_number)]) > 1:
+                raise ValidationError('The Activity Registration Number value must be unique!')
 
 
 
